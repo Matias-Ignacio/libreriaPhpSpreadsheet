030b72c0b738e0a69b34cf7a06df02dd2e3fac28
Matias-Ignacio <116597566+Matias-Ignacio@users.noreply.github.com>
2023-10-13 00:12:20 -0300

    Update configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -5,7 +5,7 @@ header ("Cache-Control: no-cache, must-revalidate ");
 // CONFIGURACION APP//
 /////////////////////////////
 
-$PROYECTO ='PhpSpreadsheet';
+$PROYECTO ='LibreriaPhpSpreadsheet';
 
 //variable que almacena el directorio del proyecto
 $ROOT =$_SERVER['DOCUMENT_ROOT']."/".$PROYECTO."/";
