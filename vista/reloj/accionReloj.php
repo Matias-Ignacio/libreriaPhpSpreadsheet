890f0a9be50f1ca99153ba2a39804c36a91dfa98
Matias-Ignacio <matias.riveiro@fi.uncoma.edu.ar>
2023-10-20 11:06:57 -0300

    update

--- vista/reloj/accionReloj.php
+++ vista/reloj/accionReloj.php
@@ -5,17 +5,6 @@
     $hoja = "Relojes";
     include '../funciones/crearHC.php';
 
-<<<<<<< HEAD
-
-    $datoss =data_submitted();
-    $datos = $datoss;
-
-    $resp=false; 
-    $objReloj=new AbmReloj();
-    $listaObj = $objReloj->buscar(null);
-
-=======
->>>>>>> 39d56d55c2737517b9667e5b79447bd303cdc75c
     $datos =data_submitted();
     
     $resp=false; 
